b187b4029e8154acb24f510ac8b9ca3f8777ef5a
Helena <ux24ii401@ux.edu.mx>
2026-04-23 08:32:03 -0600

    segunda version

--- src/main/java/com/ux/edu/conversational/ia/plugandplay/Main.java
+++ src/main/java/com/ux/edu/conversational/ia/plugandplay/Main.java
@@ -2,15 +2,11 @@ package com.ux.edu.conversational.ia.plugandplay;
 
 public class Main {
     public static void main(String[] args) {
-        AgenteConversacional miAgente = new AgenteConversacional();
 
-        // 1. Usamos Llama3 para una tarea compleja
-        miAgente.setModelo(new Llama3Strategy());
-        miAgente.interactuar("Explica la física cuántica.");
-        System.out.println("---");
+        InteligenciaArtificialStrategy estrategia = new Phi3Strategy();
 
-        // 2. Cambiamos a Mistral en tiempo de ejecución por su rapidez
-        miAgente.setModelo(new MistralStrategy());
-        miAgente.interactuar("Dime un chiste corto.");
+        AgenteConversacional agente = new AgenteConversacional(estrategia);
+
+        System.out.println(agente.responder("Hola"));
     }
 }
